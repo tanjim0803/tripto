f2e8fedcec23eab88db11e1f9251776199330da4
tanjim0803 <tanjimahmed1905@gmail.com>
2026-08-20 15:21:08 +0600

    added base and user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,15 @@
+from app.models.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(50), nullable=False, unique=True, index=True
+    )
+    image: Mapped[str] = mapped_column(nullable=False)
+    email_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
